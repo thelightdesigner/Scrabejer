56572c65adfbf0f625caa51dc60aa704f410a48d
thelightdesigner <48635344+thelightdesigner@users.noreply.github.com>
2023-10-19 13:21:35 -0400

    no dictionary no crash

--- Scrabejer/Form1.Designer.cs
+++ Scrabejer/Form1.Designer.cs
@@ -87,7 +87,6 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(535, 280);
             this.listBox1.TabIndex = 3;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // pictureBox3
             // 
--- Scrabejer/Form1.cs
+++ Scrabejer/Form1.cs
@@ -2,23 +2,28 @@ namespace Scrabejer
 {
     public partial class Form1 : Form
     {
-        private readonly ScrabbleDictionary dictionary = new ScrabbleDictionary();
+
+        public const string VER = "v1.0.0";
+
+        private readonly ScrabbleDictionary dictionary;
         public Form1()
         {
             InitializeComponent();
+            try
+            {
+                dictionary = new ScrabbleDictionary();
+            }
+            catch
+            {
+                MessageBox.Show("No dictionary.json file found!");
+                Environment.Exit(-1);
+            }
             ResizeRedraw = true;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
             SetValidWords();
-
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
         }
         private void SetValidWords()
         {
@@ -31,56 +36,55 @@ namespace Scrabejer
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-
             SetValidWords();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Text = $"Scrabejer {VER}";
         }
 
 
-      /*  private void button2_Click(object sender, EventArgs e)
-        {
-            tableLayoutPanel1.BackColor = Color.White;
-            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
-        } */
+        /*  private void button2_Click(object sender, EventArgs e)
+          {
+              tableLayoutPanel1.BackColor = Color.White;
+              tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+          } */
 
-       /* private void button1_Click(object sender, EventArgs e)
-        {
-            tableLayoutPanel1.Size += new Size(100,100);
+        /* private void button1_Click(object sender, EventArgs e)
+         {
+             tableLayoutPanel1.Size += new Size(100,100);
 
-        } */
+         } */
 
-      /*  private void button3_Click(object sender, EventArgs e)
-        {
+        /*  private void button3_Click(object sender, EventArgs e)
+          {
 
-            Button button = new Button();
-            button.Text = "add colums";
-            button.BackColor = Color.White;
-            
-            tableLayoutPanel1.Controls.Add(button, 0, 0);
+              Button button = new Button();
+              button.Text = "add colums";
+              button.BackColor = Color.White;
 
-            button.Click += new EventHandler(mysteryButtonClicked);
-        } */
+              tableLayoutPanel1.Controls.Add(button, 0, 0);
 
-     /*   private void mysteryButtonClicked(object sender, EventArgs e)
-        {
-            if (sender is not Button b)
-            {
-                MessageBox.Show("dang");
-                return;
-            }
-            tableLayoutPanel1.ColumnCount += 1;
+              button.Click += new EventHandler(mysteryButtonClicked);
+          } */
 
-            Button button = new();
-            button.Text = "new";
-            button.BackColor = Color.Red;
+        /*   private void mysteryButtonClicked(object sender, EventArgs e)
+           {
+               if (sender is not Button b)
+               {
+                   MessageBox.Show("dang");
+                   return;
+               }
+               tableLayoutPanel1.ColumnCount += 1;
 
-            tableLayoutPanel1.Controls.Add(button, tableLayoutPanel1.ColumnCount - 1, 0);
+               Button button = new();
+               button.Text = "new";
+               button.BackColor = Color.Red;
+
+               tableLayoutPanel1.Controls.Add(button, tableLayoutPanel1.ColumnCount - 1, 0);
 
 
-        } */
+           } */
     }
 }
